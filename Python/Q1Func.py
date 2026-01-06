c0ddb3a02ac2f0fec654593dee04849027ae27e6
MOHD NAWAZ KHAN <khanmohdnawaz567@gmail.com>
2026-01-06 17:07:41 +0530

    introduction of html
    introduction of html

---
+++ Python/Q1Func.py
@@ -0,0 +1,4 @@
+def add(a,b):
+ return a+b
+result =(5+3)
+print(result)
---
+++ Python/Q2func.py
@@ -0,0 +1,24 @@
+# def even_odd(num):
+#     if num %2 == 0:
+#      return "Even"
+#     else:
+#        return "Odd"
+# n= int(input("enter a number"))
+# print(even_odd(n))
+# # to find maximum number
+# def find_max(*numbers):
+#    return max(numbers) 
+# print(find_max(15,67,87,90,100))
+#importance of with return and without return
+# def add(a, b):
+#     print(a + b)
+
+# result = add(5, 3)
+# print(result)
+# with return
+def add(a, b):
+    return(a + b)
+
+result = add(5, 3)
+print(result)
+
