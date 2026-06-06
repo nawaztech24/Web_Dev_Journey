4b86b58d8e628199ebbfebc9fb38626b33042a0d
MOHD NAWAZ KHAN <khanmohdnawaz567@gmail.com>
2026-06-07 01:47:01 +0530

    non-primitive

---
+++ JavaScript/non-primitive.js
@@ -0,0 +1,23 @@
+//Objects
+
+
+let student = {
+    name: "Nawaz",
+    age: 20
+};
+//Arrays
+
+
+let skills = ["Java", "Python", "JavaScript", "React"];
+
+//Functions 
+
+
+function greet(){
+console.log("Assalamualaikum");
+}
+ console.log(student);
+ console.log(skills);
+ console.log(typeof student); // object
+console.log(typeof skills);  // object
+console.log(typeof greet);   // function
---
+++ JavaScript/primitive.js
@@ -0,0 +1,12 @@
+let name = "Nawaz";      // String
+let age = 20;            // Number
+let isStudent = true;    // Boolean
+let city;                // Undefined
+let data = null;         // Null
+let big = 123456789n;    // BigInt
+let id = Symbol("id");   // Symbol 
+
+
+console.log(typeof name);      // string
+console.log(typeof age);       // number
+console.log(typeof isStudent); // boolean
